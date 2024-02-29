babb75e5d50dbbff579a39feb78de5a6e373b919
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-02-29 23:44:00 +0530

    responsiveness of linktree

--- src/Components/Linking/Linking.jsx
+++ src/Components/Linking/Linking.jsx
@@ -26,12 +26,12 @@ function Linking() {
                     backgroundColor: 'rgba(17, 25, 40, 0.5)'
                 }}></div>
             </div>
-            <div className='flex items-center h-screen w-screen max-w-7xl absolute top-0 left-0 z-30'>
+            <div className='flex md:flex-row flex-col items-center h-screen w-screen max-w-7xl absolute top-0 left-0 z-30'>
                     <div className='w-1/2 flex justify-center items-center'>
-                    <img className='z-30 rounded-full h-96 w-96 mb-3' src="/images/MyImages/Shubhpic.png" alt="" />
+                    <img className='z-30 rounded-full md:h-96 md:w-96 h-48 w-48 mb-3 mt-10' src="/images/Shubh.png" alt="" />
                     </div>
                     <div className='flex flex-col justify-center items-center w-1/2'>
-                    <p className='text-white text-[32px] font-medium text-center mb-8 font-["Poppins"]'>
+                    <p className='text-white md:text-[32px] text-[24px] font-medium text-center md:mb-8 mb-10 gap-3 font-["Poppins"]'>
                         Hey There!👋🏽
                         <span className='text-indigo-500 font-semibold'> I am {text} </span>
                         <span style={{ color: '#4F46E5' }}><Cursor /></span>
